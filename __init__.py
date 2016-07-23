59f3077e04eb60ee50c4f3cd3e4f3bc874273101
zhenruiliao@gmail.com <zhenruiliao@gmail.com>
2016-07-23 13:54:27 +0100

    Cleanup

---
+++ __init__.py
@@ -0,0 +1,8 @@
+'''
+Package providing the code generation framework.
+'''
+from brian2.codegen.statements import *
+from brian2.codegen.translation import *
+from brian2.codegen._prefs import *
+from .numba_rt import *
+from .numba_generator import *
